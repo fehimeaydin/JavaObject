b31205dbaab8a5b195e3d3dec23983d67418dc3c
Fehime <Fehime@Fehime.hacettepe.edu.tr>
2018-12-29 01:41:12 +0300

    Güncelleme

--- src/faydin/obj/Odev231218/Runner.java
+++ src/faydin/obj/Odev231218/Runner.java
@@ -49,13 +49,40 @@ public class Runner {
 		
 		
 		ben.anne.anne = new Odev();
-		ben.anne.anne.adi= "xxxyyy";
+		ben.anne.baba = new Odev();
+		ben.anne.anne.adi= "Şükriye";
+		ben.anne.baba.adi= "İbrahim";
 		
 		
-		System.out.println(ben.adi  + " " +ben.soyadi);
-
-		System.out.println(ben.anne.adi); 
-		System.out.println(ben.baba.adi); 
+		ben.baba.anne = new Odev();
+		ben.baba.baba = new Odev();
+		ben.baba.anne.adi= "Fehime";
+		ben.baba.baba.adi= "Hüseyin";
+		
+		
+		ben.anne.anne.anne = new Odev();
+		ben.anne.anne.baba = new Odev();
+		ben.anne.baba.anne = new Odev();
+		ben.anne.baba.baba = new Odev();
+		ben.anne.anne.anne.adi = "Zeycan";
+		ben.anne.anne.baba.adi = "İhsan";
+		ben.anne.baba.anne.adi = "Zahide";
+		ben.anne.baba.baba.adi = "Şerif";
+		
+		
+		ben.baba.anne.anne = new Odev();
+		ben.baba.anne.baba = new Odev();
+		ben.baba.baba.anne = new Odev();
+		ben.baba.baba.baba = new Odev();
+		ben.baba.anne.anne.adi = "Nurten";
+		ben.baba.anne.baba.adi = "Mehmet";
+		ben.baba.baba.anne.adi = "Hanife";
+		ben.baba.baba.baba.adi = "Celil";
+		
+		System.out.println("\t\t\t"+ ben.adi  + " " +ben.soyadi);
+		System.out.println("\t"+ben.anne.adi + " " +ben.anne.soyadi +  "\t\t\t"+   ben.baba.adi + " " +ben.baba.soyadi); 
+		System.out.println("   " + ben.anne.anne.adi + "\t" +ben.anne.baba.adi +  "\t\t "+   ben.baba.anne.adi + "\t\t" + ben.baba.baba.adi); 
+		System.out.println( ben.anne.anne.anne.adi + "  " +ben.anne.anne.baba.adi +  "  "+ ben.anne.baba.anne.adi + "  " +ben.anne.baba.baba.adi + "  " + ben.baba.anne.anne.adi + "  " +ben.baba.anne.baba.adi + "  " + ben.baba.baba.anne.adi + "  "+ ben.baba.baba.baba.adi); 
 		
 		 /*************************************/
 		  
